c79dc820baf818d902b217912727a9fe0985c939
Juuso Lahtinen <juusolah@metropolia.fi>
2020-11-28 13:09:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.androidproject;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements OnTimeSetListener {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textViewAlarm);
+
+        Button buttonTimePicker = findViewById(R.id.button_timepicker);
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new com.example.androidproject.TimePicker();
+                timePicker.show(getSupportFragmentManager(),"time picker");
+            }
+        });
+
+        Button buttonCancelAlarm = findViewById(R.id.button_cancel);
+        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+
+        updateTimeText(calendar);
+        startAlarm(calendar);
+    }
+
+    private void updateTimeText(Calendar calendar) {
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime());
+
+        textView.setText(timeText);
+    }
+
+    private void startAlarm(Calendar calendar) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent  = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        if (calendar.before(Calendar.getInstance())) {
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent  = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.cancel(pendingIntent);
+        textView.setText("alarm cancelled");
+    }
+}
---
+++ app/src/main/java/com/example/androidproject/NotificationHelper.java
@@ -0,0 +1,48 @@
+package com.example.androidproject;
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.provider.Settings;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationHelper extends ContextWrapper {
+
+    public static final String channel_ID = "channel ID";
+    public static final String channel_name = "channel name";
+
+    private NotificationManager manager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public void createChannel() {
+            NotificationChannel channel = new NotificationChannel(channel_ID, channel_name, NotificationManager.IMPORTANCE_HIGH);
+
+            getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if (manager == null) {
+            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return manager;
+    }
+
+    public NotificationCompat.Builder getNotification() {
+        return new NotificationCompat.Builder(getApplicationContext(), channel_ID)
+                .setContentTitle("Herätyskello")
+                .setContentText("Toimii")
+                .setSmallIcon(R.drawable.ic_baseline_add_alarm_24);
+    }
+}
